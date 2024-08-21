5825c51f13e01f714c485afb51fccca8ce2312bb
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-08-21 14:40:57 -0700

    fix footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,39 +5,10 @@ export default function Footer() {
 	const currentYear = new Date().getFullYear();
 
 	return (
-		<footer className="bg-gray-500 flex justify-between text-white py-3 px-2">
-			<p className="block">
-				&copy; {currentYear} Beauty Market. All rights reserved.
+		<footer className="bg-gray-500 flex justify-center text-white py-3 px-2">
+			<p className="text-sm text-center">
+				&copy; {currentYear} Natalia Salon. Todos los derechos reservados.
 			</p>
-			<ul className="flex gap-5 items-center">
-				<li>
-					<a
-						href="https://whatsapp.com"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<FaWhatsapp />
-					</a>
-				</li>
-				<li>
-					<a
-						href="https://facebook.com"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<FaFacebook />
-					</a>
-				</li>
-				<li>
-					<a
-						href="https://instagram.com"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<FaInstagram />
-					</a>
-				</li>
-			</ul>
 		</footer>
 	);
 }
