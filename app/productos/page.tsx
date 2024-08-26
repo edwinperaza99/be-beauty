4529a90af747e66d4bd3dd4dca568d9cc9fa7ea3
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-08-25 22:23:55 -0700

    set basic styles for product page

--- app/productos/page.tsx
+++ app/productos/page.tsx
@@ -1,8 +1,10 @@
+import "../index.css";
+
 export default function Productos() {
 	return (
 		<>
 			<main className="">
-				<section className="flex flex-col items-center" id="landing">
+				<section className="flex flex-col items-center" id="products">
 					<h1 className="text-4xl font-bold">Nuestros mejores productos</h1>
 					<p className="text-lg">
 						Bienvenido al lugar donde encontraras todos los productos que
