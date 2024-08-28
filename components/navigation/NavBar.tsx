34da7af2c4152ee31c8902044dd051b569f8de4a
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-08-28 15:55:51 -0700

    make navbar responsive

--- components/navigation/NavBar.tsx
+++ components/navigation/NavBar.tsx
@@ -42,6 +42,7 @@ export default function NavBar() {
 						</Link>
 					</li>
 				</ul>
+				{/* social media links */}
 				<ul className="hidden md:flex gap-2 justify-end mr-4">
 					<li>
 						<a href="" className="p-2">
@@ -112,7 +113,7 @@ export default function NavBar() {
 
 				{/* hamburger menu button  */}
 				<button
-					className="group h-11 w-11 rounded-lg border-white flex justify-center items-center border-2"
+					className="md:hidden group h-11 w-11 rounded-lg border-white flex justify-center items-center border-2"
 					onClick={toggleNavbar}
 				>
 					<div className="grid place-content-center gap-2">
@@ -133,6 +134,96 @@ export default function NavBar() {
 						></span>
 					</div>
 				</button>
+
+				{/* mobile menu when toggled  */}
+				{isNavbarOpen && (
+					<div className="absolute top-full left-0 w-full bg-black/30">
+						<ul className="flex flex-col justify-center text-center pt-4 gap-2">
+							<li>
+								<Link href="/servicios" className="p-4 hover:text-green-300">
+									Servicios
+								</Link>
+							</li>
+							<li>
+								<Link href="/productos" className="p-4 hover:text-green-300">
+									Productos
+								</Link>
+							</li>
+							<li>
+								<Link href="/staff" className="p-4 hover:text-green-300">
+									Staff
+								</Link>
+							</li>
+						</ul>
+						<ul className="flex gap-2 justify-end mr-4">
+							<li>
+								<a href="" className="p-2">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="24"
+										height="24"
+										viewBox="0 0 24 24"
+										fill="none"
+										stroke="currentColor"
+										strokeWidth="2"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										className="icon icon-tabler icons-tabler-outline icon-tabler-brand-instagram hover:text-green-300"
+									>
+										<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+										<path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" />
+										<path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+										<path d="M16.5 7.5l0 .01" />
+									</svg>
+								</a>
+							</li>
+							<li>
+								<a href="" className="p-2">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="24"
+										height="24"
+										viewBox="0 0 24 24"
+										fill="none"
+										stroke="currentColor"
+										strokeWidth="2"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										className="icon icon-tabler icons-tabler-outline icon-tabler-brand-facebook hover:text-green-300"
+									>
+										<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+										<path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+									</svg>
+								</a>
+							</li>
+							<li>
+								<a
+									href="https://api.whatsapp.com/send/?phone=50372951072&text&type=phone_number&app_absent=0"
+									className="p-2"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="24"
+										height="24"
+										viewBox="0 0 24 24"
+										fill="none"
+										stroke="currentColor"
+										strokeWidth="2"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										className="icon icon-tabler icons-tabler-outline icon-tabler-brand-whatsapp hover:text-green-300"
+									>
+										<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+										<path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
+										<path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1" />
+									</svg>
+								</a>
+							</li>
+						</ul>
+					</div>
+				)}
 			</nav>
 		</header>
 	);
