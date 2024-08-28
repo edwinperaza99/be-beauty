da78c494b4a3b24ad1d99de206e74e8ac786769f
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-08-28 16:53:26 -0700

    adjust footer text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
 
 	return (
 		<footer className="bg-black flex justify-center text-white py-3 px-2">
-			<p className="text-sm text-center">
+			<p className="text-xs md:text-sm text-center">
 				&copy; {currentYear} Natalia Salon. Todos los derechos reservados.
 			</p>
 		</footer>
