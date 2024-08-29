d546782d38685021a67f9a9d6b6da846e91d0be0
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-08-29 11:31:58 -0700

    clean file

--- components/map/Map.tsx
+++ components/map/Map.tsx
@@ -1,7 +1,3 @@
-"use client";
-
-import React from "react";
-
 export default function Map() {
 	return (
 		<div>
@@ -9,11 +5,8 @@ export default function Map() {
 				src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3876.5779450495147!2d-89.22488782491095!3d13.683408686701403!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f63304c923b5363%3A0x2fc17f7826216b18!2sNatalia%20Salon!5e0!3m2!1sen!2sus!4v1724954562634!5m2!1sen!2sus"
 				width="600"
 				height="450"
-				// style="border:0;"
-				// allowfullscreen=""
 				loading="lazy"
 				className="w-full h-[200px]"
-				// referrerpolicy="no-referrer-when-downgrade"
 			></iframe>
 		</div>
 	);
