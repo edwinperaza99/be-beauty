672c62122f4ac67105369d512b960a294bd94919
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-08-28 18:43:22 -0700

    make dynamic

--- components/Contactanos.tsx
+++ components/Contactanos.tsx
@@ -19,8 +19,8 @@ export function Contactanos() {
 			<AccordionItem value="item-1">
 				<AccordionTrigger>Ubicación</AccordionTrigger>
 				<AccordionContent>
-					<h3>Autopista Sur</h3>
-					<p>
+					<h3 className="text-md">Autopista Sur</h3>
+					<p className="text-xs pt-0.5">
 						Dirección: Centro Comercial Autopista Sur, Local #43, San Salvador
 					</p>
 				</AccordionContent>
@@ -28,8 +28,8 @@ export function Contactanos() {
 			<AccordionItem value="item-2">
 				<AccordionTrigger>Contacto</AccordionTrigger>
 				<AccordionContent className="flex flex-col gap-2">
-					<p>Telefono: +503 2248-0105</p>
-					<ul className="flex gap-4">
+					<p className="text-xs md:text-md">Telefono: +503 2248-0105</p>
+					<ul className="flex gap-2 md:gap-4">
 						<li>
 							<Popover>
 								<PopoverTrigger>
@@ -167,7 +167,7 @@ export function Contactanos() {
 			<AccordionItem value="item-3">
 				<AccordionTrigger>Horario</AccordionTrigger>
 				<AccordionContent>
-					<ul>
+					<ul className="text-[0.6rem] md:text-md">
 						<li>Lunes - Sábado: 7:00 AM - 7:00 PM</li>
 						{/* <li>Lunes: 9am - 5pm</li>
                         <li>Martes: 9am - 5pm</li>
