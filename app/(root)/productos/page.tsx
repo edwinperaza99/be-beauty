97f7ebf3a89d1388c342ae9620f3a93a30c149d5
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-09-04 15:59:31 -0700

    get all products

--- app/(root)/productos/page.tsx
+++ app/(root)/productos/page.tsx
@@ -1,15 +1,54 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Product } from "@/prisma/client";
 import "../index.css";
 
 export default function Productos() {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		fetch("/api/products", {
+			method: "GET",
+		})
+			.then((response) => response.json())
+			.then((data) => setProducts(data))
+			.catch((error) => console.error("Error fetching products:", error));
+	}, []);
 	return (
 		<>
 			<main className="">
-				<section className="flex flex-col items-center" id="products">
+				<section className="flex flex-col items-center">
 					<h1 className="text-4xl font-bold">Nuestros mejores productos</h1>
 					<p className="text-lg">
 						Bienvenido al lugar donde encontraras todos los productos que
 						necesitas
 					</p>
+				</section>
+				{/* products cards  */}
+				<section className="container max-w-fit grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-6 py-6">
+					{products.length > 0 ? (
+						products.map((product: Product) => (
+							<article
+								key={product.id}
+								className="h-62 w-52 border border-sm rounded-sm p-4"
+							>
+								<div className="h-40">
+									<img
+										src={product.image_url}
+										alt="${product.name} image"
+										className="w-full h-full object-cover"
+									/>
+								</div>
+								<div className="mt-4">
+									<h2 className="text-lg">{product.name}</h2>
+									<p className="text-sm truncate">{product.description}</p>
+								</div>
+							</article>
+						))
+					) : (
+						<p>No products available</p>
+					)}
 				</section>
 			</main>
 		</>
