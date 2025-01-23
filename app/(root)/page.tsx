fbabb82ef1a15c1e846965b80854798e60586deb
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2025-01-23 05:33:18 -0800

    add animations with framer and migrate to tailwindcss

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,13 @@
 import { Sentimiento } from "@/components/AnimatedSVG";
 import { Contactanos } from "@/components/Contactanos";
+import {
+	fadeInOut,
+	MotionDiv,
+	MotionH2,
+	MotionP,
+	MotionSection,
+	slideInFromBottom,
+} from "@/components/motionUtils";
 import salon_inside from "@/public/salon.jpg";
 import Image from "next/image";
 import Link from "next/link";
@@ -9,18 +17,40 @@ export default function Home() {
 		<>
 			<main className="overflow-hidden" id="start">
 				<section
-					className="flex flex-col justify-center landing-bg gap-2 mx-auto my-0"
+					className="flex flex-col justify-center landing-bg gap-2 mx-auto my-0 min-h-dvh w-full relative"
 					id="landing"
 				>
-					<div className="hero">
-						<p className="text-4xl">Es más que cabello,</p>
-						<p className="text-4xl">
+					<div className="flex flex-col px-2 md:px-0 min-w-0 md:min-w-[800px] md:mx-auto">
+						<MotionP
+							variants={fadeInOut}
+							initial="hidden"
+							whileInView="visible"
+							viewport={{ once: false, amount: 0.5 }}
+							transition={{ duration: 0.8, delay: 0.2 }}
+							className="text-4xl md:text-5xl mb-1"
+						>
+							Es más que cabello,
+						</MotionP>
+						<MotionP
+							variants={fadeInOut}
+							initial="hidden"
+							whileInView="visible"
+							viewport={{ once: false, amount: 0.5 }}
+							transition={{ duration: 0.8, delay: 0.3 }}
+							className="text-4xl md:text-5xl mb-6"
+						>
 							Es un
 							<span className="pl-2 inline-block">
-								<Sentimiento width="170px" height="auto" />
+								<Sentimiento width="190px" height="auto" />
 							</span>
-						</p>
-						<div>
+						</MotionP>
+						<MotionDiv
+							variants={slideInFromBottom}
+							initial="hidden"
+							whileInView="visible"
+							viewport={{ once: false, amount: 0.5 }}
+							transition={{ duration: 0.8, delay: 0.6 }}
+						>
 							<Link
 								href="https://api.whatsapp.com/send/?phone=50372951072&text&type=phone_number&app_absent=0"
 								target="_blank"
@@ -29,28 +59,52 @@ export default function Home() {
 							>
 								Haz tu cita
 							</Link>
-						</div>
+						</MotionDiv>
 					</div>
 				</section>
-				<section
-					id="services"
-					className="flex flex-col justify-center py-16 gap-4 px-4"
-				>
-					<h2 className="text-center text-4xl">SERVICIOS</h2>
+				<section className="flex flex-col justify-center py-16 gap-4 px-4 mx-auto max-w-4xl">
+					<MotionH2
+						variants={fadeInOut}
+						initial="hidden"
+						whileInView="visible"
+						viewport={{ once: true, amount: 0.4 }}
+						transition={{ duration: 0.8, delay: 0.6 }}
+						className="text-center text-4xl"
+					>
+						SERVICIOS
+					</MotionH2>
 					<div className="text-center text-sm md:text-md">
-						<p>
+						<MotionP
+							variants={fadeInOut}
+							initial="hidden"
+							whileInView="visible"
+							viewport={{ once: true, amount: 0.4 }}
+							transition={{ duration: 0.8, delay: 0.6 }}
+						>
 							Queremos ser parte de tus momentos, tu estilo transforma tu look
 							por completo. En Natalia Salon sabemos que cada momento tiene su
 							estilo y cuenta una historia.
-						</p>
-						<p>
+						</MotionP>
+						<MotionP
+							variants={fadeInOut}
+							initial="hidden"
+							whileInView="visible"
+							viewport={{ once: true, amount: 0.4 }}
+							transition={{ duration: 0.8, delay: 0.6 }}
+						>
 							Ya sea un liso más brillante , un color más radiante, tus manos y
 							pies impecable y más ¡Impresiona y lúce con el poder de un nuevo
 							look!
-						</p>
+						</MotionP>
 					</div>
 					<article className="grid grid-cols-3">
-						<div>
+						<MotionDiv
+							variants={slideInFromBottom}
+							initial="hidden"
+							whileInView="visible"
+							viewport={{ once: true, amount: 0.3 }}
+							transition={{ duration: 0.8, delay: 0.3 }}
+						>
 							<Image
 								src="https://images.pexels.com/photos/3993311/pexels-photo-3993311.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
 								alt="teñido de pelo"
@@ -59,8 +113,14 @@ export default function Home() {
 								className="p-2 md:p-4"
 							/>
 							<h3 className="text-xl text-center font-black">Color</h3>
-						</div>
-						<div>
+						</MotionDiv>
+						<MotionDiv
+							variants={slideInFromBottom}
+							initial="hidden"
+							whileInView="visible"
+							viewport={{ once: true, amount: 0.3 }}
+							transition={{ duration: 0.8, delay: 0.4 }}
+						>
 							<Image
 								src="https://images.pexels.com/photos/3997391/pexels-photo-3997391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
 								alt="Manicure"
@@ -69,8 +129,14 @@ export default function Home() {
 								className="p-2 md:p-4"
 							/>
 							<h3 className="text-xl text-center font-black">Uñas</h3>
-						</div>
-						<div>
+						</MotionDiv>
+						<MotionDiv
+							variants={slideInFromBottom}
+							initial="hidden"
+							whileInView="visible"
+							viewport={{ once: true, amount: 0.3 }}
+							transition={{ duration: 0.8, delay: 0.5 }}
+						>
 							<Image
 								src="https://images.pexels.com/photos/3993453/pexels-photo-3993453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
 								alt="Corte de pelo"
@@ -79,67 +145,68 @@ export default function Home() {
 								className="p-2 md:p-4"
 							/>
 							<h3 className="text-xl text-center font-black">Corte</h3>
-						</div>
+						</MotionDiv>
 					</article>
-					<div className="flex justify-center">
+					<MotionDiv
+						variants={slideInFromBottom}
+						initial="hidden"
+						whileInView="visible"
+						viewport={{ once: true, amount: 0.3 }}
+						transition={{ duration: 0.5, delay: 0.2 }}
+						className="flex justify-center"
+					>
 						<Link
 							href="/servicios"
 							className="py-2 px-8 bg-[#CCD580] text-white hover:bg-[#191b16] transition-colors"
 						>
 							Más
 						</Link>
-					</div>
+					</MotionDiv>
 				</section>
 				<section className="bg-[#242921] flex justify-center items-center relative w-full h-[400px] md:h-[650px]">
-					{/* <div className="p-4 z-0 max-w-[800px] h-[300px]"> */}
 					<Image
 						src={salon_inside}
-						// src="../public/salon_inside.jpg"
 						alt=""
-						// layout="fill"
-						// objectFit="cover"
-						// height={450}
 						width={800}
-						className="absolute z-0 opacity-80 phrase-section-image"
+						placeholder="blur"
+						className="px-4 md:px:0 absolute z-0 opacity-20 hover:opacity-25 object-cover object-center max-h-[80%] transition-opacity"
 					/>
-					{/* </div> */}
-					<p className="absolute text-3xl md:text-5xl text-center place-content-center z-10 text-[#CCD580]">
+					<MotionDiv
+						variants={fadeInOut}
+						initial="hidden"
+						whileInView="visible"
+						viewport={{ once: true, amount: 0.7 }}
+						transition={{ duration: 0.8, delay: 0.4 }}
+						className="absolute text-3xl md:text-5xl text-center place-content-center z-10 text-[#CCD580]"
+					>
 						Be Beauty, <wbr /> Be You
-					</p>
+					</MotionDiv>
 				</section>
 				<div className="bg-[#191b16] h-12 md:h-20"></div>
-				<section className="px-4 bg-[#242921] text-white py-8">
-					<h2 className="text-4xl text-center py-2">CONTACTANOS</h2>
-					<article className="flex gap-4 md:gap-8 text-sm md:text-md space-between items-center contactanos min-h-[457px] md:min-h-[420px]">
-						<div className="contact-image-container">
-							<Image
-								src="https://images.pexels.com/photos/3993453/pexels-photo-3993453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-								alt="Corte de pelo"
-								height={300}
-								width={300}
-								// fill
-								// objectFit="cover"
-								className=""
-							/>
-						</div>
-						<Contactanos />
-					</article>
-				</section>
-				{/* <section id="brands">
-					<h2 className="text-4xl text-center">NUESTRAS MARCAS</h2>
-					<article className="grid grid-cols-3">
-						<Image src="/brand1.jpg" alt="Brand 1" width={300} height={300} />
-						<Image src="/brand2.jpg" alt="Brand 2" width={300} height={300} />
-						<Image src="/brand3.jpg" alt="Brand 3" width={300} height={300} />
-						<Image src="/brand4.jpg" alt="Brand 4" width={300} height={300} />
-						<Image src="/brand5.jpg" alt="Brand 5" width={300} height={300} />
-						<Image src="/brand6.jpg" alt="Brand 6" width={300} height={300} />
-					</article>
-				</section>
-				<section id="contact" className="bg-green-900">
-					<h2 className="text-4xl">Contactanos</h2>
-					<div className="flex"></div>
-				</section> */}
+				<div className="bg-[#242921]">
+					<MotionSection
+						variants={fadeInOut}
+						initial="hidden"
+						whileInView="visible"
+						viewport={{ once: true, amount: 0.3 }}
+						transition={{ duration: 0.8, delay: 0.4 }}
+						className="px-4 bg-[#242921] text-white py-8"
+					>
+						<h2 className="text-4xl text-center py-2">CONTACTANOS</h2>
+						<article className="flex gap-4 md:gap-8 text-sm md:text-md space-between items-center max-w-4xl mx-auto">
+							<div className="contact-image-container overflow-hidden">
+								<Image
+									src="https://images.pexels.com/photos/3993453/pexels-photo-3993453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+									alt="Corte de pelo"
+									height={300}
+									width={300}
+									className=""
+								/>
+							</div>
+							<Contactanos />
+						</article>
+					</MotionSection>
+				</div>
 			</main>
 		</>
 	);
